685306a60697dfddb57b647ccca5da5f69aa9e69
sK1 Project <sk1project@users.noreply.github.com>
2016-10-19 22:13:46 +0300

    makemsi build for sk1

---
+++ sk1.mm
@@ -0,0 +1,6 @@
+#define VER_FILENAME.VER  sk1.Ver
+
+#include "SK1.MMH"
+
+<$DirectoryTree Key="INSTALLDIR" Dir="c:\program files\sK1 Project\sK1" CHANGE="\" PrimaryFolder="Y">
+<$Files "msi_build\*.*" DestDir="INSTALLDIR" SubDir="TREE">
