3cb105946b36e1eba8d7822ea779253412dc399f
sK1 Project <sk1project@users.noreply.github.com>
2016-10-20 01:49:45 +0300

    app shortcut added

--- sk1.mm
+++ sk1.mm
@@ -4,3 +4,19 @@
 
 <$DirectoryTree Key="INSTALLDIR" Dir="c:\program files\sK1 Project\sK1" CHANGE="\" PrimaryFolder="Y">
 <$Files "msi_build\*.*" DestDir="INSTALLDIR" SubDir="TREE">
+
+<$Component "AdvertisedShortcut" Create="Y" Directory_="INSTALLDIR">
+    ;--- Add the files to the "TryMe" component -----------------------------
+    <$File SOURCE="msi_build\sk1.exe" KeyPath="Y">
+
+    #(
+        <$Shortcut
+                   Dir="ProgramMenuFolder"
+               Feature="."
+                 Title="sK1 illustration program"
+           Description=^sK1 illustration program^
+                  Icon="@.\sk1.ico"
+               WorkDir="INSTALLDIR"
+        >
+    #)
+<$/Component>
