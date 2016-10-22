29febbf8032516f7081af6e4b8a2e7af16360162
sK1 Project <sk1project@users.noreply.github.com>
2016-10-22 21:17:55 +0300

    fixes for correct paths

--- sk1.mm
+++ sk1.mm
@@ -2,7 +2,7 @@
 
 #include "SK1.MMH"
 
-<$DirectoryTree Key="INSTALLDIR" Dir="c:\program files\sK1 Project\sK1" CHANGE="\" PrimaryFolder="Y">
+<$DirectoryTree Key="INSTALLDIR" Dir="[ProgramFilesFolder]\sK1 Project\sK1" CHANGE="\" PrimaryFolder="Y">
 <$Files "msi_build\*.*" DestDir="INSTALLDIR" SubDir="TREE">
 
 <$Component "AdvertisedShortcut" Create="Y" Directory_="INSTALLDIR">
