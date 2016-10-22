1d92b54425348efcb047a6b731c2c5a724960e6b
sK1 Project <sk1project@users.noreply.github.com>
2016-10-22 21:17:32 +0300

    64bit msi build

---
+++ sk1_x64.mm
@@ -0,0 +1,22 @@
+#define VER_FILENAME.VER  sk1.Ver
+
+#include "SK1.MMH"
+
+<$DirectoryTree Key="INSTALLDIR" Dir="[ProgramFiles64Folder]\sK1 Project\sK1" CHANGE="\" PrimaryFolder="Y">
+<$Files "msi_build\*.*" DestDir="INSTALLDIR" SubDir="TREE">
+
+<$Component "AdvertisedShortcut" Create="Y" Directory_="INSTALLDIR">
+    ;--- Add the files to the "TryMe" component -----------------------------
+    <$File SOURCE="msi_build\sk1.exe" KeyPath="Y">
+
+    #(
+        <$Shortcut
+                   Dir="ProgramMenuFolder"
+               Feature="."
+                 Title="sK1 illustration program"
+           Description=^sK1 illustration program^
+                  Icon="@.\sk1.ico"
+               WorkDir="INSTALLDIR"
+        >
+    #)
+<$/Component>
